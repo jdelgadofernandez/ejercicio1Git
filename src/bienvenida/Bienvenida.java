f535edb78a4c85c3f1e31d7d53becfe9e8441c7e
am291092 <am291092@gmail.com>
2023-04-18 09:45:18 +0200

    Cambios antonio

--- src/bienvenida/Bienvenida.java
+++ src/bienvenida/Bienvenida.java
@@ -3,7 +3,7 @@ package bienvenida;
 public class Bienvenida {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Se te ha olvidado jose");
 
 	}
 
